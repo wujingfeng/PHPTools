c65d28242f17e6e48b911185c4f0cf7748d58a2d
吴靖丰 <3002143185@qq.com>
2019-05-10 17:35:03 +0800

    Signed-off-by: 吴靖丰 <3002143185@qq.com>

--- demo.php
+++ demo.php
@@ -6,18 +6,28 @@ require_once "./vendor/autoload.php";
 use FileTools\ImageTools;
 use FileTools\PHPExcelTools;
 use GeneralTools\StringTools;
-
-
+use FileTools\FileTools;
+use GeneralTools\OtherTools;
+//use Others\Sorts;
+use GeneralTools\Sorts;
 //ImageTool::index();
 //$result = StringTools::to_guid_string(6);
 //print_r($result);
 
 //PHPExcelTools::export();
-PHPExcelTools::index();
-$e = new PHPExcel();
-$c = $e->getActiveSheet();
-//print_r($c);
+//PHPExcelTools::index();
+//$e = new PHPExcel();
+//$c = $e->getActiveSheet();
+////print_r($c);
+//
+//$a = new PHPExcel_Writer_Excel5($e);
+//$b=$a->getDiskCachingDirectory();
+//print_r($b);
 
-$a = new PHPExcel_Writer_Excel5($e);
-$b=$a->getDiskCachingDirectory();
-print_r($b);
+//FileTools::autoDownloadPic();
+//$res = OtherTools::getCyclingDistance();
+//print($res);
+
+//Sorts::BubbleSort([3,4,2,5,1,6]);
+//Sorts::selectSort([3,4,2,5,1,6]);
+Sorts::insertSort([3,4,2,5,1,6]);
--- src/GeneralTools/OtherTools.php
+++ src/GeneralTools/OtherTools.php
@@ -3,8 +3,6 @@
 
 namespace GeneralTools;
 
-use GeneralTools\RequestTools;
-
 class OtherTools
 {
     /*
@@ -18,7 +16,7 @@ class OtherTools
         if (strpos($ip, '192.168.') !== false || $ip == '127.0.0.1') {
             return '本地';
         }
-        $url = "http://ip.taobao.com/service/getIpInfo.php?ip=".$ip;
+        $url = "http://ip.taobao.com/service/getIpInfo.php?ip=" . $ip;
 
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
@@ -29,7 +27,7 @@ class OtherTools
         $result = curl_exec($curl);
         curl_close($curl);
 
-        $result = json_decode($result,true);
+        $result = json_decode($result, true);
         if ($result != false) {
             if ($result['code']) {
                 return '未知';
@@ -49,7 +47,7 @@ class OtherTools
      * @param $key
      * @return float
      */
-    public static function getCyclingDistance($point1="22,154", $point2="22,88",$key='aa2e60a76c1f3b574114762da8b7645b')
+    public static function getCyclingDistance($point1 = "22,154", $point2 = "22,88", $key = 'aa2e60a76c1f3b574114762da8b7645b')
     {
         $distance = 0.00;
         if (empty($point1) || empty($point2)) {
@@ -64,10 +62,8 @@ class OtherTools
             'destination' => $point2,
         ];
         $params = http_build_query($params);
-        $url = "http://restapi.amap.com/v4/direction/bicycling?".$params;
-        print_r($url);
-        $data = RequestTools::http_get($url,10);
-        print_r($data);
+        $url = "http://restapi.amap.com/v4/direction/bicycling?" . $params;
+        $data = RequestTools::http_get($url);
         if (isset($data) && $data['errcode'] == 0) {
             if (isset($data['data']['paths'][0]['distance']) && $data['data']['paths'][0]['distance'] > 0) {
                 $distance = $data['data']['paths'][0]['distance'] <= 100
@@ -79,5 +75,90 @@ class OtherTools
             $distance = self::getLineDistance($point1, $point2);
         }
         return $distance;
+    }
+
+    /**
+     * 获取两个坐标点的驾车距离
+     * @param $point1
+     * @param $point2
+     * @param $key
+     * @return float
+     */
+    public static function getDrivingDistance($point1, $point2, $key)
+    {
+        $distance = 0.00;
+        if (empty($point1) || empty($point2)) {
+            return $distance;
+        }
+        if (!strpos($point1, ',') || !strpos($point2, ',')) {
+            return $distance;
+        }
+        $params = [
+            'key' => $key,
+            'origin' => $point1,
+            'destination' => $point2,
+            'strategy' => 2,       // 距离优先，不考虑路况，仅走距离最短的路线，但是可能存在穿越小路/小区的情况
+            'extensions' => 'base',// base:返回基本信息；all：返回全部信息
+        ];
+        $params = http_build_query($params);
+        $url = "http://restapi.amap.com/v4/direction/bicycling?" . $params;
+        $data = RequestTools::http_get($url);
+        if ($data && 1 == $data['status']) {
+            if (isset($data['route']['paths'][0]['distance']) && $data['route']['paths'][0]['distance'] > 0) {
+                $distance = $data['route']['paths'][0]['distance'] <= 100
+                    ? 0.01 :
+                    round($data['route']['paths'][0]['distance'] / 1000, 2);
+            }
+        }
+        if ($distance <= 0) {
+            $distance = self::getLineDistance($point1, $point2);
+        }
+        return $distance;
+    }
+
+    /**
+     * 获取两点之前的步行距离
+     * @param $point1
+     * @param $point2
+     * @param $key
+     * @return float
+     */
+    public static function getWalkingDistance($point1, $point2, $key)
+    {
+        $distance = 0.00;
+        if (empty($point1) || empty($point2)) {
+            return $distance;
+        }
+        if (!strpos($point1, ',') || !strpos($point2, ',')) {
+            return $distance;
+        }
+        $params = [
+            'key' => $key,
+            'origin' => $point1,
+            'destination' => $point2,
+        ];
+        $params = http_build_query($params);
+        $url = "http://restapi.amap.com/v4/direction/bicycling?" . $params;
+        $data = RequestTools::http_get($url, 10);
+        if ($data && 1 == $data['status']) {
+            if (isset($data['route']['paths'][0]['distance']) && $data['route']['paths'][0]['distance'] > 0) {
+                $distance = $data['route']['paths'][0]['distance'] <= 100
+                    ? 0.01 :
+                    round($data['route']['paths'][0]['distance'] / 1000, 2);
+            }
+        }
+        if ($distance <= 0) {
+            $distance = self::getLineDistance($point1, $point2);
+        }
+        return $distance;
+    }
+
+    public static function getDistanceByType($point1, $point2, $key,$type){
+        switch ($type){
+            case 1:self::getWalkingDistance($point1, $point2, $key);
+            case 2:self::getCyclingDistance($point1, $point2, $key);
+            case 3:self::getDrivingDistance($point1, $point2, $key);
+            case 4:self::getDistanceByType($point1, $point2, $key);
+        }
     }
 }
